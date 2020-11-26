8a7fa7701dd81b8e2b70e9b6eea8896cfa25f07b
段体华 <duantihua@gmail.com>
2020-11-26 12:35:29 +0800

    增加注释

--- src/main/scala/org/openurp/std/creditbank/web/helper/CertificateGradePropertyExtractor.scala
+++ src/main/scala/org/openurp/std/creditbank/web/helper/CertificateGradePropertyExtractor.scala
@@ -18,11 +18,38 @@
  */
 package org.openurp.std.creditbank.web.helper
 
-import java.time.{LocalDate, ZoneId}
 import java.time.format.DateTimeFormatter
+import java.time.{LocalDate, ZoneId}
 
 import org.beangle.data.transfer.exporter.DefaultPropertyExtractor
 
+/** 证书成绩输出辅助类
+ * 转换属性如下：
+ * <table  class="striped" style="text-align:left">
+ * <thead><tr><td>属性</td><td>标题</td><td>备注</td></tr></thead>
+ * <tbody>
+ * <tr><td>1. 姓名</td><td>grade.std.user.name</td><td></td></tr>
+ * <tr><td>2. 身份证号</td><td>grade.std.person.code</td><td></td></tr>
+ * <tr><td>3. 转换学校代码</td><td>schoolCode</td><td>本学校所在的学分银行分配的代码</td></tr>
+ * <tr><td>4. 原课程来源代码</td><td>original.course.code</td><td>01 课程类 02 证书类</td></tr>
+ * <tr><td>5. 原专业名称</td><td>grade.major.name</td><td>不涉及专业,待补</td></tr>
+ * <tr><td>6. 原课程名称</td><td>grade.subject.name</td><td></td></tr>
+ * <tr><td>7. 原办学机构</td><td>grade.subject.institutionName</td><td></td></tr>
+ * <tr><td>8. 原教育层次代码</td><td>original.level.code</td><td></td></tr>
+ * <tr><td>9. 原教育类别代码</td><td>original.project.category.code</td><td></td></tr>
+ * <tr><td>10. 原学分</td><td>original.course.credits</td><td>以0代替</td></tr>
+ * <tr><td>11. 原学时</td><td>original.course.creditHours</td><td>以0代替</td></tr>
+ * <tr><td>12. 原成绩</td><td>grade.scoreText</td><td></td></tr>
+ * <tr><td>13. 获得时间</td><td>grade.acquiredOn</td<td></td></tr>
+ * <tr><td>14. 转换后教育层次代码</td><td>grade.std.level.code</td><td>20 本科 21 专科 22 专升本 23 高起本</td></tr>
+ * <tr><td>15. 转换后课程代码</td><td>course.code</td><td></td></tr>
+ * <tr><td>16. 转换后课程名称</td><td>course.name</td><td></td></tr>
+ * <tr><td>17. 转换后学分</td><td>course.credits</td><td></td></tr>
+ * <tr><td>18. 转换时间</td><td>grade.convertOn</td><td></td></tr>
+ * <tr><td>19. 转换后专业</td><td>grade.std.state.major.name</td><td></td></tr>
+ * </tbody>
+ * </table>
+ */
 class CertificateGradePropertyExtractor extends DefaultPropertyExtractor {
 
   private val yearMonth = DateTimeFormatter.ofPattern("YYYYMM")
@@ -31,11 +58,13 @@ class CertificateGradePropertyExtractor extends DefaultPropertyExtractor {
     val data = target.asInstanceOf[CertificateData]
     property match {
       case "original.course.code" => "02"
-      case "original.level.code" => "20"
-      case "original.project.category.code" => ""
-      case "original.course.credits" => ""
-      case "original.course.creditHours" => ""
-      case "grade.convertOn"=> LocalDate.ofInstant(data.grade.updatedAt,ZoneId.systemDefault()).format(yearMonth)
+      case "original.level.code" => "20" //由于获取证书的层次未知，目前暂定20
+      case "original.project.category.code" => data.grade.std.project.category.code
+      case "original.course.credits" => "0"
+      case "original.course.creditHours" => "0"
+      case "grade.major.name" => "待补"
+      case "schoolCode" => ""
+      case "grade.convertOn" => LocalDate.ofInstant(data.grade.updatedAt, ZoneId.systemDefault()).format(yearMonth)
       case "grade.acquiredOn" => data.grade.acquiredOn.format(yearMonth)
       case _ => super.getPropertyValue(target, property)
     }
--- src/main/scala/org/openurp/std/creditbank/web/helper/ExchangeGradePropertyExtractor.scala
+++ src/main/scala/org/openurp/std/creditbank/web/helper/ExchangeGradePropertyExtractor.scala
@@ -23,6 +23,33 @@ import java.time.{LocalDate, ZoneId}
 
 import org.beangle.data.transfer.exporter.DefaultPropertyExtractor
 
+/** 交换成绩输出辅助类
+ * 转换属性如下：
+ * <table  class="striped" style="text-align:left">
+ * <thead><tr><td>属性</td><td>标题</td><td>备注</td></tr></thead>
+ * <tbody>
+ * <tr><td>1. 姓名</td><td>grade.exchangeStudent.std.user.name</td><td></td></tr>
+ * <tr><td>2. 身份证号</td><td>grade.exchangeStudent.std.person.code</td><td></td></tr>
+ * <tr><td>3. 转换学校代码</td><td>schoolCode</td><td></td></tr>
+ * <tr><td>4. 原课程来源代码</td><td>original.course.code</td><td>01 课程类 02 证书类</td></tr>
+ * <tr><td>5. 原专业名称</td><td>grade.exchangeStudent.majorName</td><td></td></tr>
+ * <tr><td>6. 原课程名称</td><td>grade.courseName</td><td></td></tr>
+ * <tr><td>7. 原办学机构</td><td>grade.exchangeStudent.school.name</td><td></td></tr>
+ * <tr><td>8. 原教育层次代码</td><td>grade.exchangeStudent.level.code</td><td></td></tr>
+ * <tr><td>9. 原教育类别代码</td><td>grade.exchangeStudent.category.code</td><td></td></tr>
+ * <tr><td>10. 原学分</td><td>grade.credits</td><td></td></tr>
+ * <tr><td>11. 原学时</td><td>grade.creditHours</td><td></td></tr>
+ * <tr><td>12. 原成绩</td><td>grade.scoreText</td><td></td></tr>
+ * <tr><td>13. 获得时间</td><td>grade.acquiredOn</td><td></td></tr>
+ * <tr><td>14. 转换后教育层次代码</td><td>grade.exchangeStudent.std.level.code</td><td>20 本科 21 专科 22 专升本 23 高起本</td></tr>
+ * <tr><td>15. 转换后课程代码</td><td>course.code</td><td></td></tr>
+ * <tr><td>16. 转换后课程名称</td><td>course.name</td><td></td></tr>
+ * <tr><td>17. 转换后学分</td><td>course.credits</td><td></td></tr>
+ * <tr><td>18. 转换时间</td><td>grade.convertOn</td><td></td></tr>
+ * <tr><td>19. 转换后专业名称</td><td>grade.exchangeStudent.std.state.major.name</td><td></td></tr>
+ * </tbody>
+ * </table>
+ */
 class ExchangeGradePropertyExtractor extends DefaultPropertyExtractor {
 
   private val yearMonth = DateTimeFormatter.ofPattern("YYYYMM")
@@ -30,10 +57,11 @@ class ExchangeGradePropertyExtractor extends DefaultPropertyExtractor {
   override def getPropertyValue(target: Object, property: String): Any = {
     val data = target.asInstanceOf[ExchangeGradeData]
     property match {
-      case "grade.courseCode" => "03"
-      case "grade.creditHours" => ""
+      case "original.course.code" => "01"
+      case "grade.creditHours" => "0"
       case "grade.convertOn" => LocalDate.ofInstant(data.grade.updatedAt, ZoneId.systemDefault()).format(yearMonth)
       case "grade.acquiredOn" => data.grade.acquiredOn.format(yearMonth)
+      case "schoolCode" => ""
       case _ => super.getPropertyValue(target, property)
     }
   }
