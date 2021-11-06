121d99ec56fb893630b55dcc13d3ced238243ab1
段体华 <duantihua@gmail.com>
2021-11-06 16:37:41 +0800

    rebase on scala 3.1.0

---
+++ build.sbt
@@ -0,0 +1,45 @@
+import org.openurp.parent.Settings._
+import org.openurp.parent.Dependencies._
+import org.beangle.tools.sbt.Sas
+
+ThisBuild / organization := "org.openurp.std.creditbank"
+ThisBuild / version := "0.0.5"
+
+ThisBuild / scmInfo := Some(
+  ScmInfo(
+    url("https://github.com/openurp/std-creditbank"),
+    "scm:git@github.com:openurp/std-creditbank.git"
+  )
+)
+
+ThisBuild / developers := List(
+  Developer(
+    id    = "chaostone",
+    name  = "Tihua Duan",
+    email = "duantihua@gmail.com",
+    url   = url("http://github.com/duantihua")
+  )
+)
+
+ThisBuild / description := "OpenURP Std CreditBank"
+ThisBuild / homepage := Some(url("http://openurp.github.io/std-creditbank/index.html"))
+
+val apiVer = "0.23.3"
+val starterVer = "0.0.12"
+val baseVer = "0.1.21"
+val openurp_edu_api = "org.openurp.edu" % "openurp-edu-api" % apiVer
+val openurp_std_api = "org.openurp.std" % "openurp-std-api" % apiVer
+val openurp_stater_web = "org.openurp.starter" % "openurp-starter-web" % starterVer
+val openurp_base_tag = "org.openurp.base" % "openurp-base-tag" % baseVer
+
+lazy val root = (project in file("."))
+  .enablePlugins(WarPlugin)
+  .settings(
+    name := "openurp-creditbank-adminapp",
+    common,
+    libraryDependencies ++= Seq(openurp_stater_web,openurp_base_tag),
+    libraryDependencies ++= Seq(openurp_edu_api,openurp_std_api,beangle_ems_app),
+    libraryDependencies ++= Seq(Sas.Tomcat % "test")
+  )
+
+publish / skip := true
--- src/main/scala/org/openurp/std/creditbank/web/action/ExternAction.scala
+++ src/main/scala/org/openurp/std/creditbank/web/action/ExternAction.scala
@@ -1,28 +1,27 @@
 /*
- * OpenURP, Agile University Resource Planning Solution.
- *
- * Copyright © 2014, The OpenURP Software.
+ * Copyright (C) 2005, The OpenURP Software.
  *
  * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful.
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+ * GNU Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
+ * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 package org.openurp.std.creditbank.web.action
 
 import java.time.ZoneId
 import org.beangle.data.dao.OqlBuilder
 import org.beangle.data.transfer.exporter.ExportSetting
-import org.beangle.webmvc.api.view.View
-import org.beangle.webmvc.entity.action.{EntityAction, ExportSupport}
+import org.beangle.web.action.view.View
+import org.beangle.webmvc.support.action.{EntityAction, ExportSupport}
 import org.openurp.starter.edu.helper.ProjectSupport
 import org.openurp.edu.extern.model.ExternGrade
 import org.openurp.std.creditbank.web.helper.{ExternGradeData, ExternGradePropertyExtractor}
